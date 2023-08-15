ff589969c5b0a8055e534d2692a5e42ba4bcc6a0
Kunal Verma <kunalvrm555@gmail.com>
2023-08-15 17:20:38 +0530

    fix: error handling, prevent exit
    - Fixed error handling for various scenarios in the message handler.
    - Added try-catch blocks to handle exceptions and prevent the program from exiting.
    - Improved error logging and added error messages to the appropriate responses.
    - Updated endpoint handlers to send appropriate HTTP status codes for success and failure.
    - Added catch-all middleware to handle unhandled routes and return a 404 status code.

--- mftp.js
+++ mftp.js
@@ -1,28 +1,31 @@
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+
+// Middleware setup to parse incoming request bodies
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(
-    bodyParser.urlencoded({
-        limit: '50mb',
-        extended: true,
-        parameterLimit: 50000,
-    })
-);
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(bodyParser.text({ limit: '200mb' }));
 
+// Static chat ID
 const chatId = '919354817605@c.us';
 
+// Import necessary modules
 const { Client, MessageMedia, LocalAuth } = require('./index');
 const qrcode = require('qrcode-terminal');
+
+// Initialize the client with specific options
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true, args: ['--no-sandbox'] },
 });
+
 client.initialize();
+
+// Event handlers for the WhatsApp client
+
+// Generate QR code for authentication
 client.on('qr', (qr) => {
-    // NOTE: This event will not be fired if a session is specified.
     console.log('QR RECEIVED');
     qrcode.generate(qr, { small: true });
 });
@@ -32,7 +35,6 @@ client.on('authenticated', () => {
 });
 
 client.on('auth_failure', (msg) => {
-    // Fired if session restore was unsuccessful
     console.error('AUTHENTICATION FAILURE', msg);
 });
 
@@ -43,96 +45,107 @@ client.on('ready', () => {
 client.on('disconnected', (reason) => {
     console.log('Client was logged out', reason);
 });
+
+// Handling incoming messages
 client.on('message', async (message) => {
-    //a new message is received
-    const chat = await message.getChat(); //obtain chat details in which message is received
-    if (message.body == 'hi') {
-        chat.sendMessage('Hello');
-    } else if (message.body.startsWith('add ')) {
+    try {
         const chat = await message.getChat();
-        if (chat.id._serialized == chatId) {
-            const groupId = '120363148894935956@g.us';
-            try {
-                const group = await client.getChatById(groupId);
-                const number = message.body.split(' ')[1];
-                const participantId = `91${number}@c.us`;
-                console.log(participantId);
-                const result = await group.addParticipants([participantId]);
-                console.log(result);
-                message.reply(result[participantId].message);
-            } catch (error) {
-                console.error('Error while adding participant:', error);
-                message.reply(error.message);
+        if (message.body == 'hi') {
+            chat.sendMessage('Hello');
+        } else if (message.body.startsWith('add ')) {
+            if (chat.id._serialized == chatId) {
+                const groupId = '120363148894935956@g.us';
+                try {
+                    const group = await client.getChatById(groupId);
+                    const number = message.body.split(' ')[1];
+                    const participantId = `91${number}@c.us`;
+                    const result = await group.addParticipants([participantId]);
+                    message.reply(result[participantId].message);
+                } catch (error) {
+                    console.error('Error while adding participant:', error);
+                    message.reply(error.message);
+                }
+            } else {
+                chat.sendMessage('You are not authorized to add participants');
             }
-        } else {
-            chat.sendMessage('You are not authorized to add participants');
         }
+    } catch (error) {
+        console.error('Error in message handler:', error);
     }
 });
 
-// Server port
+// Server configuration
 var HTTP_PORT = 8000;
-// Start server
+
+// Start the server
 app.listen(HTTP_PORT, () => {
     console.log('Server running on port %PORT%'.replace('%PORT%', HTTP_PORT));
 });
-// Root endpoint
+
+// Basic endpoint to test if API is working
 app.get('/', (req, res) => {
     res.json({ message: 'API ENDPOINT WORKING' });
 });
 
-// Insert here other API endpoints
+// Endpoint to send message
 app.post('/sendmessage/', async (req, res) => {
-    var data = {
-        message: req.body.message,
-    };
-    await client.sendMessage(chatId, data.message);
-    const chats = await client.getChats();
-    // console.log(chats);
-    chats.forEach(async (chat) => {
-        if (chat.isGroup) {
-            chat.sendMessage(data.message);
-            // console.log('sent to chat', chat.id);
-        }
-    });
-    res.sendStatus(200);
+    try {
+        var data = { message: req.body.message };
+        await client.sendMessage(chatId, data.message);
+        const chats = await client.getChats();
+        chats.forEach(async (chat) => {
+            if (chat.isGroup) {
+                chat.sendMessage(data.message);
+            }
+        });
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error in sendmessage endpoint:', error);
+        res.sendStatus(500);
+    }
 });
 
+// Function to send media message
 const sendmedia = async (string_base64, caption_text) => {
-    const media = new MessageMedia(
-        'application/pdf',
-        string_base64,
-        caption_text
-    );
-    media.filename = 'attachment.pdf';
-    await client.sendMessage(chatId, media, {
-        caption: caption_text,
-        attachment: media,
-        sendMediaAsDocument: true,
-    });
-    const chats = await client.getChats();
-    chats.forEach(async (chat) => {
-        if (chat.isGroup) {
-            await chat.sendMessage(media, {
-                caption: caption_text,
-                attachment: media,
-                sendMediaAsDocument: true,
-            });
-            // console.log('sent to chat', chat.id);
-        }
-    });
+    try {
+        const media = new MessageMedia('application/pdf', string_base64, caption_text);
+        media.filename = 'attachment.pdf';
+        await client.sendMessage(chatId, media, {
+            caption: caption_text,
+            attachment: media,
+            sendMediaAsDocument: true,
+        });
+        const chats = await client.getChats();
+        chats.forEach(async (chat) => {
+            if (chat.isGroup) {
+                await chat.sendMessage(media, {
+                    caption: caption_text,
+                    attachment: media,
+                    sendMediaAsDocument: true,
+                });
+            }
+        });
+    } catch (error) {
+        console.error('Error in sendmedia:', error);
+    }
 };
 
+// Endpoint to send a file
 app.post('/sendfile/', (req, res) => {
-    var data = {
-        base64string: req.body.base64string,
-        caption: req.body.caption,
-    };
-    // console.log(data.base64string.slice(0, 50));
-    setTimeout(sendmedia, 500, data.base64string, data.caption);
-    res.sendStatus(200);
+    try {
+        var data = {
+            base64string: req.body.base64string,
+            caption: req.body.caption,
+        };
+        setTimeout(sendmedia, 500, data.base64string, data.caption);
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error in sendfile endpoint:', error);
+        res.sendStatus(500);
+    }
 });
 
+// Catch-all middleware for unhandled routes
 app.use(function (req, res) {
     res.status(404);
 });
